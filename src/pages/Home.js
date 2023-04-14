efa2cd465d02829c4b7e8aa214f324a2a5d990f1
Pixel90Mex <118486482+Pixel90Mex@users.noreply.github.com>
2023-04-14 21:11:20 +0200

    Add files via upload

---
+++ src/pages/Home.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Navigation from '../components/Navigation'
+import Counter from '../components/Counter'
+import Hero from '../components/Hero'
+import Main from '../components/Main'
+import Footer from '../components/Footer'
+
+const Home = () => {
+  return (
+   <>
+   <Navigation/>
+   <Counter/>
+   <Hero/>
+   <Main/>
+   <Footer/>
+   </>
+  )
+}
+
+export default Home
